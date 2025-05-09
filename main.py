2bed4c4f8c23c55781fa806751fef77bf8883e12
hloly01 <137940807+hloly01@users.noreply.github.com>
2025-05-09 15:53:40 -0400

    Fully documented!

--- main.py
+++ main.py
@@ -8,15 +8,19 @@ from XRPLib.timeout import *
 # Initialize HuskyLens on I2C and differential drive system
 husky = HuskyLensLibrary("I2C")
 differentialDrive = DifferentialDrive.get_default_differential_drive()
-# c = connect_mqtt()
-# c.ping()
+c = connect_mqtt()
+if c == None: 
+    MQTT = 0
+    print("MQTT failed to connect")
+else: 
+    MQTT = 1
+    c.ping()
 
 # Ensure HuskyLens is in color recognition mode
 while not husky.color_recognition_mode():
     husky.color_recognition_mode()
 
-
-# Main loop
+# STAGE 1: move forward for 5 seconds to demonstrate walking robots
 def test_walk():
     print("walk!")
     timer = Timeout(5) # start timer for 30 sec
@@ -26,6 +30,7 @@ def test_walk():
     print("done walking :)")
     drivetrain.set_speed(0,0)
 
+# STAGE 2: track person using color tracking and proportional control. 
 def follow_person():
     base_velocity = 15
     P = 0.2
@@ -47,15 +52,18 @@ def follow_person():
             y = (state_x2 + state_x3) / 2
             update = 1
 
-            if y < 68:
-                base_velocity = max(10 * math.sqrt(abs(68 - y)), 15)
+            # use position in screen to determine robot movement. y determines forward motion, x determines turning
+
+            if y < 68: # object in top of screen, move forward 
+                # choose a base velocity that reflects how far the person is
+                base_velocity = -1*max(10 * math.sqrt(abs(68 - y)), 15)
                 print(f"base velocity: {base_velocity:.2f}")
 
                 # --- WRAPAROUND DETECTION ---
                 if previous_x is not None and previous_x > 200 and x < 60:
                     print(f"wraparound detected → turn right({base_velocity+delta_velocity, base_velocity - delta_velocity})")
-                    # delta_velocity = P * 100
                     differentialDrive.set_speed(base_velocity + delta_velocity, base_velocity - delta_velocity)
+                    # don't update previous state if there is a wraparound
                     update = 0 
                 
                 # --- NORMAL TRACKING ---
@@ -73,11 +81,13 @@ def follow_person():
                     delta_velocity = P * (x - 160)
                     differentialDrive.set_speed(base_velocity + delta_velocity, base_velocity - delta_velocity)
 
+            # object in center of screen, wait to proceed
             elif 68 <= y <= 75:
                 differentialDrive.set_speed(0.0, 0.0)
                 base_velocity = 0
                 delta_velocity = 0
-
+            
+            # object too close to robot, back up
             elif y > 75:
                 print("back up")
                 differentialDrive.set_speed(-12.0, -12.0)
@@ -85,25 +95,27 @@ def follow_person():
                 delta_velocity = 0
 
             print(f"x0: {state_x0} \tx1: {state_x1} \tx2: {state_x2} \tx3: {state_x3}")
-            state_history.append([state_x0, state_x1, state_x2, state_x3])
-            # c.publish("topic/HONK_Results", str([state_x0, state_x1, state_x2, state_x3]), retain=True)
+            if MQTT: # If connected to MQTT, publish results 
+                state_history.append([state_x0, state_x1, state_x2, state_x3])
+                c.publish("topic/HONK_Results", str([state_x0, state_x1, state_x2, state_x3]), retain=True)
 
-            # if len(state_history) > 10:
-            #     for i in range(1, 11):
-            #         c.publish(f"topic/HONK_Results_{i}", str(state_history[-i]), retain=True)
+                if len(state_history) > 10:
+                    for i in range(1, 11):
+                        c.publish(f"topic/HONK_Results_{i}", str(state_history[-i]), retain=True)
 
             if update: 
                 previous_x = x  # Save last x
 
         else:
-            if repeat < 70:
+            # move forward for 5 seconds in an attempt to relocate person. then stop
+            if repeat < 50:
                 differentialDrive.set_speed(base_velocity, base_velocity)
                 print(f"repeat {repeat}")
                 repeat += 1
                 time.sleep(0.1)
             differentialDrive.set_speed(0.0, 0.0)
-
         time.sleep(0.1)
 
+# run the code! 
 test_walk()
 follow_person()
